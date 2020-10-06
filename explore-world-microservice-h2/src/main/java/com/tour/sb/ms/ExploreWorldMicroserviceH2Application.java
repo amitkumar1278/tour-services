36bc7dd6a99a3c41fb701c77862d1eec2f077b35
AMIT KUMAR <amitkumar12788@gmail.com>
2020-10-07 01:48:28 +0530

    updated

---
+++ explore-world-microservice-h2/src/main/java/com/tour/sb/ms/ExploreWorldMicroserviceH2Application.java
@@ -0,0 +1,14 @@
+package com.tour.sb.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreWorldMicroserviceH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreWorldMicroserviceH2Application.class, args);
+	}
+	
+
+}
