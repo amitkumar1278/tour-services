6ba1a055e03982590504b39b7036d1092cd70216
AMIT KUMAR <amitkumar12788@gmail.com>
2020-10-07 01:49:11 +0530

    added

---
+++ explore-world-microservice-h2/src/test/java/com/tour/sb/ms/ExploreWorldMicroserviceH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tour.sb.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreWorldMicroserviceH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
