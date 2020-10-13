d0d4e39c09729a447fb73ff095069867e77e8fae
amitkumar12788 <amitkumar12788@gmail.com>
2020-10-13 09:03:24 +0530

    updated

--- explore-world-ms-sdr-h2/src/main/java/com/tour/sb/ms/web/TourRatingController.java
+++ explore-world-ms-sdr-h2/src/main/java/com/tour/sb/ms/web/TourRatingController.java
@@ -3,10 +3,22 @@
  */
 package com.tour.sb.ms.web;
 
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tour.sb.ms.domain.Tour;
+import com.tour.sb.ms.domain.TourRating;
+import com.tour.sb.ms.domain.TourRatingPK;
 import com.tour.sb.ms.repo.TourRatingRepository;
 import com.tour.sb.ms.repo.TourRepository;
 
@@ -34,4 +46,39 @@ public class TourRatingController {
 
 	}
 
+    /**
+     * Create a Tour Rating.
+     *
+     * @param tourId tour identifier
+     * @param ratingDto rating data transfer object
+     */
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void createTourRating(@PathVariable(value = "tourId") int tourId,
+			@RequestBody @Validated RatingDto ratingDto) {
+		
+		Tour tour = verifyTour(tourId);
+		tourRatingRepository.save(new TourRating(new TourRatingPK(tour, ratingDto.getCustomerId()),
+				ratingDto.getScore(), ratingDto.getComment()));
+	}
+
+    /**
+     * Verify and return the Tour given a tourId.
+     *
+     * @param tourId tour identifier
+     * @return the found Tour
+     * @throws NoSuchElementException if no Tour found.
+     */
+	private Tour verifyTour(int tourId) {
+		// TODO Auto-generated method stub
+		return tourRepository.findById(tourId)
+				.orElseThrow(() -> new NoSuchElementException("Tour does not exist :" + tourId));
+	}
+
+	
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(NoSuchElementException.class)
+	public String return400(NoSuchElementException ex) {
+		return ex.getMessage();
+	}
 }
