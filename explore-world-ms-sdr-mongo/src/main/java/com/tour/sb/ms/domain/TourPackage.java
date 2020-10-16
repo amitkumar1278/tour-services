04ebc66feb31f5021508f5872e3177f256440a0f
amitkumar12788 <amitkumar12788@gmail.com>
2020-10-17 01:00:20 +0530

    updated

--- explore-world-ms-sdr-mongo/src/main/java/com/tour/sb/ms/domain/TourPackage.java
+++ explore-world-ms-sdr-mongo/src/main/java/com/tour/sb/ms/domain/TourPackage.java
@@ -3,8 +3,6 @@
  */
 package com.tour.sb.ms.domain;
 
-import java.io.Serializable;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
