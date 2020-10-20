f7425ea4bff09491da770b0f8e808dca0d903c8b
amitkumar12788 <amitkumar12788@gmail.com>
2020-10-21 02:02:35 +0530

    updated

---
+++ explorePlaces-services-sdr-h2/src/test/java/com/example/ec/service/TourRatingServiceTest.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.example.ec.service;
+
+/**
+ * @author amit
+ *
+ */
+public class TourRatingServiceTest {
+
+}
---
+++ explorePlaces-services-sdr-h2/src/test/java/com/example/ec/web/RatingControllerTest.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.example.ec.web;
+
+/**
+ * @author amit
+ *
+ */
+public class RatingControllerTest {
+
+}
