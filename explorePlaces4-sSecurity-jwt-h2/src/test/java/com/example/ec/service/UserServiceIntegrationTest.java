8d865f2b66951684bbb8154871c24c241469e2aa
amitkumar12788 <amitkumar12788@gmail.com>
2020-10-27 11:31:14 +0530

    updated

--- explorePlaces4-sSecurity-jwt-h2/src/test/java/com/example/ec/service/UserServiceIntegrationTest.java
+++ explorePlaces4-sSecurity-jwt-h2/src/test/java/com/example/ec/service/UserServiceIntegrationTest.java
@@ -1,45 +1,29 @@
-/**
- * 
- */
 package com.example.ec.service;
 
-import static org.hamcrest.CoreMatchers.not;
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.assertj.core.api.Assertions.not;
-//import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertThat;
-
-import java.util.Optional;
-
-import javax.transaction.Transactional;
-
+import com.example.ec.domain.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.example.ec.domain.User;
+import java.util.Optional;
 
-/**
- * @author amit
- *
- */
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
 public class UserServiceIntegrationTest {
-
-	@Autowired
-	private UserService service;
-	
-	@SuppressWarnings("deprecation")
-	@Test
-	public void signup() {
-		Optional<User> user = service.signup("dummyUsername", "dummypassword", "john", "doe");
-		
-		assertThat(user.get().getPassword(), not("dummypassword"));
-		System.out.println("Encoded Password = "+user.get().getPassword());
-	}
+    @Autowired
+    private UserService service;
+    @Test
+    public void signup() {
+        Optional<User> user = service.signup("dummyUsername", "dummypassword", "john", "doe");
+        assertThat(user.get().getPassword(), not("dummypassword"));
+        System.out.println("Encoded Password = " + user.get().getPassword());
+    }
 }
